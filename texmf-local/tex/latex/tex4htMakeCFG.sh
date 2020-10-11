eedcff9a20d1d9ec25662b988bdf10e9c9d53a4c
llorracc <Christopher.Carroll@jhu.edu>
2020-10-11 17:24:32 -0400

    makePublic autoupate

--- texmf-local/tex/latex/tex4htMakeCFG.sh
+++ texmf-local/tex/latex/tex4htMakeCFG.sh
@@ -10,12 +10,6 @@ handoutName=$1
 
 # cd "$(dirname "$0")" # http://stackoverflow.com/questions/3349105/how-to-set-current
 
-cmd="cp `kpsewhich svg-math-and-subfigures.cfg` $handoutName.cfg"
-echo "$cmd" ; eval "$cmd"
-#cp `kpsewhich svg-set-size-to-1p0.mk4` $handoutName.mk4
-cmd="cp `kpsewhich svg-set-size-to-1p0.mk4` $handoutName.mk4"
-#cp `kpsewhich svg-set-size-to-1p2.mk4` $handoutName.mk4
-#cmd="cp `kpsewhich svg-set-size-to-1p3x1p0.mk4` $handoutName.mk4".mk4
-echo "$cmd" ; eval "$cmd"
-
+cp `kpsewhich svg-math-and-subfigures.cfg` $handoutName.cfg
+cp `kpsewhich svg-set-size-to-1p0.mk4` $handoutName.mk4
 
