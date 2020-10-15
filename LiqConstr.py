60e0fdeb61c3be3009423406e34251778c11b565
llorracc <Christopher.Carroll@jhu.edu>
2020-10-15 17:50:21 -0400

    makePublic autoupate

--- LiqConstr.py
+++ LiqConstr.py
@@ -94,7 +94,7 @@ from HARK.ConsumptionSaving.ConsIndShockModel import IndShockConsumerType
 # Common parameters for all models (the initialized lifecycle perfect foresight type with no borrowing constraint)
 
 # load default parameteres from the lifecycle model in the HARK toolbox
-from HARK.ConsumptionSaving.ConsumerParameters import init_lifecycle
+from HARK.ConsumptionSaving.ConsIndShockModel import init_lifecycle
 
 # remove all risk and growth factors, borrowing constraints, and set the solver to always use linear interpolation
 init_lifecycle["PermGroFac"] = [1,1,1,1,1,1,1,1,1,1]
@@ -145,20 +145,20 @@ CCC_unconstr = IndShockConsumerType(**init_lifecycle)
 CCC_unconstr.delFromTimeInv('BoroCnstArt')
 CCC_unconstr.addToTimeVary('BoroCnstArt')
 CCC_unconstr.solve()
-CCC_unconstr.unpackcFunc()
+CCC_unconstr.unpack('cFunc')
 
 CCC_constraint = IndShockConsumerType(**init_lifecycle)
 CCC_constraint.delFromTimeInv('BoroCnstArt')
 CCC_constraint.addToTimeVary('BoroCnstArt')
 CCC_constraint.BoroCnstArt = [None,-1,None,None,None,None,None,None,None,None]
 CCC_constraint.solve()
-CCC_constraint.unpackcFunc()
+CCC_constraint.unpack('cFunc')
 
 CCC_risk = IndShockConsumerType(**init_lifecycle_risk1)
 CCC_risk.delFromTimeInv('BoroCnstArt')
 CCC_risk.addToTimeVary('BoroCnstArt')
 CCC_risk.solve()
-CCC_risk.unpackcFunc()
+CCC_risk.unpack('cFunc')
 
 # save the data in a txt file for later plotting in Matlab
 x = np.linspace(-1,1,500,endpoint=True)
@@ -217,7 +217,7 @@ Bcons1.delFromTimeInv('BoroCnstArt')
 Bcons1.addToTimeVary('BoroCnstArt')
 Bcons1.BoroCnstArt = [None,0,None,None,None,None,None,None,None,None]
 Bcons1.solve()
-Bcons1.unpackcFunc()
+Bcons1.unpack('cFunc')
 
 # Make and solve the consumer with more than one binding borrowing constraint
 BCons2 = IndShockConsumerType(**init_lifecycle)
@@ -225,7 +225,7 @@ BCons2.delFromTimeInv('BoroCnstArt')
 BCons2.addToTimeVary('BoroCnstArt')
 BCons2.BoroCnstArt = [None,0,0.02,None,None,None,None,None,None,None]
 BCons2.solve()
-BCons2.unpackcFunc()
+BCons2.unpack('cFunc')
 
 # save the data in a txt file
 x = np.linspace(1,1.2,500,endpoint=True)
@@ -286,13 +286,13 @@ WwCR_unconstr = IndShockConsumerType(**init_lifecycle)
 WwCR_unconstr.delFromTimeInv('BoroCnstArt')
 WwCR_unconstr.addToTimeVary('BoroCnstArt')
 WwCR_unconstr.solve()
-WwCR_unconstr.unpackcFunc()
+WwCR_unconstr.unpack('cFunc')
 
 WwCR_risk = IndShockConsumerType(**init_lifecycle_risk2)
 WwCR_risk.delFromTimeInv('BoroCnstArt')
 WwCR_risk.addToTimeVary('BoroCnstArt')
 WwCR_risk.solve()
-WwCR_risk.unpackcFunc()
+WwCR_risk.unpack('cFunc')
 
 WwCR_constr = IndShockConsumerType(**init_lifecycle)
 WwCR_constr.cycles = 1 # Make this consumer live a sequence of periods exactly once
@@ -300,14 +300,14 @@ WwCR_constr.delFromTimeInv('BoroCnstArt')
 WwCR_constr.addToTimeVary('BoroCnstArt')
 WwCR_constr.BoroCnstArt = [None,None,-6,None,None,None,None,None,None,None]
 WwCR_constr.solve()
-WwCR_constr.unpackcFunc()
+WwCR_constr.unpack('cFunc')
 
 WwCR_constr_risk = IndShockConsumerType(**init_lifecycle_risk2)
 WwCR_constr_risk.delFromTimeInv('BoroCnstArt')
 WwCR_constr_risk.addToTimeVary('BoroCnstArt')
 WwCR_constr_risk.BoroCnstArt = [None,None,-6,None,None,None,None,None,None,None]
 WwCR_constr_risk.solve()
-WwCR_constr_risk.unpackcFunc()
+WwCR_constr_risk.unpack('cFunc')
 
 # save the data in a txt file
 x = np.linspace(-8,-4,1000,endpoint=True)
@@ -327,7 +327,7 @@ print('Figure 3: Consumption Functions With and Without a Constraint and a Risk'
 f = plt.figure()
 plt.plot(x,y,color="black",linewidth=3)
 plt.plot(x,y2,color="black",linestyle="--",linewidth=3)
-plt.plot(x,y3,color="black")
+plt.plot(x,y3,color="black",linestyle=":", linewidth=3)
 plt.plot(x,y4,color="black",linestyle="--")
 plt.xlim(left=-8,right=-4.5)
 plt.ylim(0,0.30)
@@ -369,21 +369,21 @@ WwCR_unconstr = IndShockConsumerType(**init_lifecycle)
 WwCR_unconstr.delFromTimeInv('BoroCnstArt')
 WwCR_unconstr.addToTimeVary('BoroCnstArt')
 WwCR_unconstr.solve()
-WwCR_unconstr.unpackcFunc()
+WwCR_unconstr.unpack('cFunc')
 
-CCC_unconstr.unpackcFunc()
+CCC_unconstr.unpack('cFunc')
 WwCR_risk = IndShockConsumerType(**init_lifecycle_risk3)
 WwCR_risk.delFromTimeInv('BoroCnstArt')
 WwCR_risk.addToTimeVary('BoroCnstArt')
 WwCR_risk.solve()
-WwCR_risk.unpackcFunc()
+WwCR_risk.unpack('cFunc')
 
 WwCR_constr = IndShockConsumerType(**init_lifecycle)
 WwCR_constr.delFromTimeInv('BoroCnstArt')
 WwCR_constr.addToTimeVary('BoroCnstArt')
 WwCR_constr(BoroCnstArt = [None,None,-6,None,None,None,None,None,None,None])
 WwCR_constr.solve()
-WwCR_constr.unpackcFunc()
+WwCR_constr.unpack('cFunc')
 
 
 WwCR_constr_risk = IndShockConsumerType(**init_lifecycle_risk3)
@@ -391,7 +391,7 @@ WwCR_constr_risk.delFromTimeInv('BoroCnstArt')
 WwCR_constr_risk.addToTimeVary('BoroCnstArt')
 WwCR_constr_risk(BoroCnstArt = [None,None,-6,None,None,None,None,None,None,None])
 WwCR_constr_risk.solve()
-WwCR_constr_risk.unpackcFunc()
+WwCR_constr_risk.unpack('cFunc')
 
 # save the data in a txt file
 x = np.linspace(-8,-4,1000,endpoint=True)
@@ -454,28 +454,28 @@ f.savefig(os.path.join(figures_dir, 'ConstrHidesRisk.svg'))
 #WwCR_unconstr.delFromTimeInv('BoroCnstArt')
 #WwCR_unconstr.addToTimeVary('BoroCnstArt')
 #WwCR_unconstr.solve()
-#WwCR_unconstr.unpackcFunc()
+#WwCR_unconstr.unpack('cFunc')
 #
 ## 4 is risk in period 4 (1)
 #WwCR_risk4 = IndShockConsumerType(**init_lifecycle_risk4)
 #WwCR_risk4.delFromTimeInv('BoroCnstArt')
 #WwCR_risk4.addToTimeVary('BoroCnstArt')
 #WwCR_risk4.solve()
-#WwCR_risk4.unpackcFunc()
+#WwCR_risk4.unpack('cFunc')
 #
 ## 5 is risk in period 3 (0.1)
 #WwCR_risk5 = IndShockConsumerType(**init_lifecycle_risk5)
 #WwCR_risk5.delFromTimeInv('BoroCnstArt')
 #WwCR_risk5.addToTimeVary('BoroCnstArt')
 #WwCR_risk5.solve()
-#WwCR_risk5.unpackcFunc()
+#WwCR_risk5.unpack('cFunc')
 #
 ## 6 is risk in period 3 and 4 (0.1, 1)
 #WwCR_risk6 = IndShockConsumerType(**init_lifecycle_risk6)
 #WwCR_risk6.delFromTimeInv('BoroCnstArt')
 #WwCR_risk6.addToTimeVary('BoroCnstArt')
 #WwCR_risk6.solve()
-#WwCR_risk6.unpackcFunc()
+#WwCR_risk6.unpack('cFunc')
 #
 ## save the data in a txt file
 #x = np.linspace(-8,-4,1000,endpoint=True)
